44aa017803063ed9ecdc61b8777c200ab3bbecb9
dityakp <aditya22may2004@gmail.com>
2025-12-12 21:19:52 +0530

    For Ci/Cd

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,16 +1,14 @@
 output "public_ip" {
-  value = aws_instance.strapi.public_ip
+  description = "Public IP address of the Strapi EC2 instance"
+  value       = aws_instance.strapi.public_ip
 }
 
 output "strapi_url" {
-  value = "http://${aws_instance.strapi.public_dns}:${var.strapi_port}"
+  description = "URL to access Strapi"
+  value       = "http://${aws_instance.strapi.public_dns}:${var.strapi_port}"
 }
 
 output "deployed_image" {
-  value = local.full_image
-}
-
-output "ec2_public_ip" {
-  description = "Public IP of the Strapi EC2 instance"
-  value       = aws_instance.strapi.public_ip
+  description = "Image that the instance will pull (repo:tag or image_uri)"
+  value       = local.full_image
 }
